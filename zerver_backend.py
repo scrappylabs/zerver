705006df884758aa4b0e3ccb9a7200a441aeec33
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2024-06-28 11:34:57 -0400

    a

--- zerver_backend.py
+++ zerver_backend.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import random
+import string
 
 app = Flask(__name__)
 
@@ -7,6 +9,34 @@ items = [
     {"id": 1, "name": "item1", "price": 10.99},
     {"id": 2, "name": "item2", "price": 12.99},
 ]
+
+# Initialize an empty array to store the values of k
+k_values = []
+
+
+def generate_unique_strings(count, length):
+    unique_strings = set()
+
+    while len(unique_strings) < count:
+        new_string = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        unique_strings.add(new_string)
+
+    return list(unique_strings)
+
+# Endpoint to get value K from the URL
+@app.route('/i', methods=['GET'])
+def get_k_value():
+    k = request.args.get('k')
+    if k:
+        # Do something with the value of k
+        k_values.append(k)
+        # Generate 180 unique strings of length 10
+        unique_strings = generate_unique_strings(180, 10)
+        # Convert the list to a comma-separated string
+        comma_separated_string = ','.join(unique_strings)
+        return comma_separated_string
+    else:
+        return "0"
 
 # Get all items
 @app.route('/items', methods=['GET'])
